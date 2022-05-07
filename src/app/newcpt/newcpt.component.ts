0a9f5947f4bc030ed61bad53c8af8476d7eb564f
daluozha <561890199@qq.com>
2022-05-07 13:16:49 +0800

    init new cpt

---
+++ src/app/newcpt/newcpt.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newcpt',
+  // templateUrl: './newcpt.component.html',
+  template:'<h1>hello world<h1>',
+  styleUrls: ['./newcpt.component.css']
+})
+export class NewcptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
